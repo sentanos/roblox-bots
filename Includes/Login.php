c9fd9688dd593d467702f5c180bf913c097e6805
FroastJ <froastj@gmail.com>
2015-03-24 14:12:57 -0400

    Added comments

--- Includes/Login.php
+++ Includes/Login.php
@@ -1,16 +1,20 @@
 <?php
-	function login(/*$username,$password*/) {
-		global $cookie;
-		global $lastLogin;
+	/*
+	
+	This logs in to a user and stores all cookies in a file (built-in feature of cURL). Just put in a path and it will create the file for you.
+	After logging in just use this file as the COOKIEFILE and COOKIEJAR of other cURL requests.
+	Make sure to put the correct details or it will go to captcha and you won't have any easy way of actually solving it.
+	
+	*/
+	function login($cookieFile,$username,$password) {
 		$login = array(
-			'userName'        => 'UCRPromoSystem',
-			'password'        => '-',
+			'userName'        => $username,
+			'password'        => $password,
 			'isCaptchaOn'     => false,
 			'challenge'       => '',
 			'captchaResponse' => ''
 		);
-		//https://www.roblox.com/Services/Secure/LoginService.asmx/js
-		$curl = curl_init('https://www.roblox.com/Services/Secure/LoginService.asmx/ValidateLogin');
+		$curl = curl_init('https://www.roblox.com/Services/Secure/LoginService.asmx/ValidateLogin'); // There are many links you can login from
 		curl_setopt_array($curl,array(
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_POST => true,
@@ -18,17 +22,16 @@
 			CURLOPT_HTTPHEADER => array(
 				'Content-Type: application/json; charset=utf-8'	
 			),
-			CURLOPT_COOKIEFILE => $cookie,
-			CURLOPT_COOKIEJAR => $cookie
+			CURLOPT_COOKIEFILE => $cookieFile,
+			CURLOPT_COOKIEJAR => $cookieFile // Actually save the cookies it's returning!
 		));
 		$response = curl_exec($curl);
 		curl_close($curl);
 		$responsearray = json_decode($response,true)['d'];
 		if ($responsearray['IsValid'] == true) {
-			$lastLogin = time();
 			return $response;
 		} else {
-			die('Login Failure! Error: '.$responsearray['Message']);
+			return 'Login Failure! Error: '.$responsearray['Message']; // You can change this to die()
 		}
 	}
 ?>
