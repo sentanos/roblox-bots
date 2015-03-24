b1a6ebe6d702f5ec65c3a45a73820239559a66c7
FroastJ <froastj@gmail.com>
2015-03-24 14:20:55 -0400

    Update http_parse_headers.php

--- Includes/http_parse_headers.php
+++ Includes/http_parse_headers.php
@@ -1,5 +1,14 @@
 <?php
-  if (!function_exists('http_parse_headers')) {
+	/*
+	
+	NOT MADE BY ME.
+	If you are getting the X-CSRF token using pecl_http http_parse_headers function, do:
+	http_parse_headers['X-Csrf-Token'];
+	With this it's: 
+	http_parse_headers['X-CSRF-TOKEN'];
+	
+	*/
+	if (!function_exists('http_parse_headers')) {
 		function http_parse_headers ($raw_headers) {
 			$headers = [];
 			
