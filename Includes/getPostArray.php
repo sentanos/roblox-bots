f43371efed3e0595c09e3263c1fc4f6c53184ce6
FroastJ <froastj@gmail.com>
2015-04-05 12:21:55 -0400

    Update getPostArray.php

--- Includes/getPostArray.php
+++ Includes/getPostArray.php
@@ -1,2 +1,35 @@
 <?php
+	/*
+	
+	These are so that some requests are valid.
+	The first one is a whitelist that only grabs required fields, the second gets everything it can find on the page.
+	
+	*/
+	function getPostArray($html,$preDefined) {
+		$doc = new DOMDocument();
+		$doc->loadHTML($html);
+		$postVars = array(
+			'__VIEWSTATE',
+			'__VIEWSTATEGENERATOR',
+			'__EVENTVALIDATION'
+		);
+		$newPost = $preDefined;
+		foreach($postVars as $var) {
+			$newPost[$var] = $doc->getElementById($var)->getAttribute('value');
+		}
+		return $newPost;
+	}
+	function getFullPostArray($html,$preDefined) {
+		$doc = new DOMDocument();
+		$doc->loadHTML($html);
+		$find = new DomXPath($doc);
+		$vars = $find->query('//input[@name]'); // Find all inputs with the "name" attribute
+		$newPost = $preDefined; // Put in predefined
+		foreach($vars as $var) {
+			if (!array_key_exists($var->getAttribute('name'),$newPost)/* Make sure the input isn't already set by predefined */) {
+				$newPost[$var->getAttribute('name')] = $var->getAttribute('value');
+			}
+		}
+		return $newPost;
+	}
 ?>
