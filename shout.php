6f22fc0a56d7f85f5801afc2638aaf7d22311e25
FroastJ <froastj@gmail.com>
2015-04-05 12:24:10 -0400

    Update shout.php

--- shout.php
+++ shout.php
@@ -1,2 +1,32 @@
 <?php
+	include_once'Includes/GetPostArray.php'
+	function shout($group,$cookie,$msg) {
+		$url = "http://www.roblox.com/My/Groups.aspx?gid=$group";
+		$curl = curl_init($url);
+		curl_setopt_array($curl,array(
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_COOKIEFILE => $cookie,
+			CURLOPT_COOKIEJAR => $cookie
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+		$nextPost = getPostArray(substr($response,curl_getinfo($curl,CURLINFO_HEADER_SIZE)),
+			array(
+				'ctl00$cphRoblox$GroupStatusPane$StatusTextBox' => $msg,
+				'ctl00$cphRoblox$GroupStatusPane$StatusSubmitButton' => 'Group Shout'	
+			)
+		);
+		$curl = curl_init($url);
+		curl_setopt_array($curl,array(
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_POST => true,
+			CURLOPT_POSTFIELDS => $nextPost,
+			CURLOPT_COOKIEFILE => $cookie,
+			CURLOPT_COOKIEJAR => $cookie
+		));
+		if (curl_exec($curl)) {
+			return "Shouted $msg.";
+		}
+		return 'Failure';
+	}
 ?>
