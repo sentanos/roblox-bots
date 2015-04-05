821fca20090c0762931b9826b50a8ee14d20c748
FroastJ <froastj@gmail.com>
2015-04-05 12:14:24 -0400

    More comments, small changes

--- changeRank.php
+++ changeRank.php
@@ -6,24 +6,27 @@
 	
 	I implemented the login and xcsrf getting by using Voiliax's concept - only getting it if it's invalid (much more efficient). 
 	
+	I'm also trying to not use globals because it's "good practice", so the function accepts A LOT of arguments.
 	*/
 	include_once'Includes/Login.php';
 	include_once'Includes/GetRoles.php'
 	include_once'Includes/http_parse_headers.php';
-	function updateRank($userId,$rank,$xcsrf='',$cookie,$rankLimit=255) {
+	$group = 18; // Change this to your group ID
+	list($ranks,$roles) = getRoleSets($group);
+	function updateRank($group,$userId,$rank,$xcsrf='',$cookie,$ranks,$roles,$rankLimit=255) {
 		if ($rankLimit && $rankLimit < 255) {
 			$currentRank = (int)simplexml_load_file("http://www.roblox.com/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRank&playerid=$userId&groupid=$group"); // Get user's rank
 			if ($rank > $rankLimit || $currentRank > $rankLimit) { // Check if the rank you are trying to change them to and their rank abide to the rank limit
 				return "Settings restrict the system from changing any rank over $rankLimit.";
 			}
 		}
-		$url = "http://www.roblox.com/groups/api/change-member-rank?groupId=$group&newRoleSetId=".getRoleSet($rank)."&targetUserId=$userId";
+		$url = "http://www.roblox.com/groups/api/change-member-rank?groupId=$group&newRoleSetId=".getRoleSet($rank)."&targetUserId=$userId"; // Get rank URL
 		$curl = curl_init($url);
 		curl_setopt_array($curl,array(
 			CURLOPT_HEADER => true,
 			CURLOPT_HTTPHEADER => array(                                                          
 				"X-CSRF-TOKEN: $xcsrf",
-				'Content-Length: 0'
+				'Content-Length: 0' // Because it's required :\\\\\\\
 			),
 			CURLOPT_POST => true,
 			CURLOPT_COOKIEFILE => $cookie,
@@ -33,10 +36,10 @@
 		$response = curl_exec($curl);
 		$headerSize = curl_getinfo($curl,CURLINFO_HEADER_SIZE);
 		$header = http_parse_headers(substr($response,0,$headerSize));
-		if ($header['Set-Cookie']) {
+		if ($header['Set-Cookie']) { // If the user is logged in it shouldn't set cookies.
 			login();
 			return updateRank($userId,$rank,$xcsrf,$cookie,$rankLimit);
-		} else if (curl_getinfo($curl, CURL_HTTP_CODE) == 403) {
+		} else if (curl_getinfo($curl, CURL_HTTP_CODE) == 403) { // 403 = XCSRF Token Validation Failed
 			$xcsrf = $header['X-CSRF-TOKEN'];
 			return updateRank($userId,$rank,$xcsrf,$cookie,$rankLimit);
 		}
@@ -47,7 +50,7 @@
 		} else {
 			$current = getRoleSet($currentRank);
 			$new = getRoleSet($rank);
-			return "Successfully changed rank of user $userId from ". $roles[$current] .' to '. $roles[$new] .'.';
+			return "Successfully changed rank of user $userId from ". $roles[$current] .' to '. $roles[$new] .'.'; // Details!
 		}
 	}
 ?>
