fb0f9f7e6aacc4b27e9f43c1e4ee0d249c4f4a76
FroastJ <froastj@gmail.com>
2015-04-05 12:07:12 -0400

    Update GetRoles.php

--- Includes/GetRoles.php
+++ Includes/GetRoles.php
@@ -16,27 +16,32 @@
 	}
 	
 	This is much faster than the following one, if ROBLOX does fix their API you should use it.
+	It returns both the regular rank array and another array that has the rank matched with its role.
+	To get this do:
+	list($ranks,$roles) = getRoleSets($group);
+	
+	If you want the best performance put in the ranks manually.
 	
 	*/
 	function getRoleSets($group) {
-		$roles = json_decode(file_get_contents("http://api.roblox.com/groups/$group"),true)['Roles']; // First, get all the group roles.
+		$roles = json_decode(file_get_contents("http://api.roblox.com/groups/$group"),true)['Roles'];
 		$ids = array();
 		$ranks = array();
 		$url = "http://www.roblox.com/Groups/group.aspx?gid=$group";
 		$doc = new DOMDocument();
-		$doc->loadHTMLFile($url); // Load up the group page (contains role ids)
-		$find = new DomXPath($doc); // XPath is love, XPath is life
-		$nodes = $find->query('//select[contains(@id,\'ctl00_cphRoblox_rbxGroupRoleSetMembersPane_dlRolesetList\')]//option[@value]'); // Get <select> that has a certain ID and then get <option>'s inside that select with a value value.
+		$doc->loadHTMLFile($url);
+		$find = new DomXPath($doc);
+		$nodes = $find->query('//select[contains(@id,\'ctl00_cphRoblox_rbxGroupRoleSetMembersPane_dlRolesetList\')]//option[@value]');
 		foreach ($nodes as $node) {
-			$ids[$node->textContent] = $node->getAttribute('value'); // For each option get its textContent (role name) and value attribute (role ID)
+			$ids[$node->textContent] = $node->getAttribute('value');
 		}
 		foreach ($ids as $name => $id) {
 			foreach ($roles as $array) {
 				if ($name == $array['Name']) {
-					$ranks[$array['Rank']] = $id; // Iterate through roles and match them with the correct ID
+					$ranks[$array['Rank']] = $id;
 				}
 			}
 		}
-		return $ranks;
+		return array($ranks,array_flip($ids));
 	}
 ?>
