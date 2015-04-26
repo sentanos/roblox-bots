57a22eb4c4be1c6ccd92deb9774d3e16225d6694
FroastJ <froastj@gmail.com>
2015-04-25 21:31:22 -0400

    Initial

---
+++ Examples/example.php
@@ -0,0 +1,17 @@
+<?php
+	// This is simply an example file so that you know how to work everything (specifically logging in and cookies).
+	include_once 'Includes/GetRoles.php';
+	include_once 'Includes/Login.php';
+	include_once 'changeRank.php';
+	libxml_use_internal_errors(true); // Hide DOMDocument errors (though your errors should be turned off anyways)
+	$group = 18; // Change this to your group ID
+	$cookieTime = 'Private/cookieTime.txt';
+	$cookie = 'Private/cookie';
+	if (time()-file_get_contents($cookieTime) > 86400) {
+		login($cookie,'Killer6199','mandie');
+		file_put_contents($cookieTime,time());
+	}
+	list($ranks,$roles) = getRoleSets($group);
+	echo updateRank($group,2470023,13,$cookie,$ranks,$roles);
+	// Update rank in group $group of user 2470023 to rank 13 using cookie file $cookie, ranks array $ranks, and roles array $roles.
+?>
