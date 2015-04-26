bd909695ea0f874f7c60329833c13c47d6aac04d
FroastJ <froastj@gmail.com>
2015-04-25 23:31:45 -0400

    Small bug fix

--- shout.php
+++ shout.php
@@ -1,6 +1,6 @@
 <?php
-	include_once'Includes/GetPostArray.php'
-	function shout($group,$cookie,$msg) {
+	include_once'Includes/GetPostArray.php';
+	function shout($cookie,$group,$msg) {
 		$url = "http://www.roblox.com/My/Groups.aspx?gid=$group";
 		$curl = curl_init($url);
 		curl_setopt_array($curl,array(
@@ -9,13 +9,13 @@
 			CURLOPT_COOKIEJAR => $cookie
 		));
 		$response = curl_exec($curl);
-		curl_close($curl);
 		$nextPost = getPostArray(substr($response,curl_getinfo($curl,CURLINFO_HEADER_SIZE)),
 			array(
 				'ctl00$cphRoblox$GroupStatusPane$StatusTextBox' => $msg,
 				'ctl00$cphRoblox$GroupStatusPane$StatusSubmitButton' => 'Group Shout'	
 			)
 		);
+		curl_close($curl);
 		$curl = curl_init($url);
 		curl_setopt_array($curl,array(
 			CURLOPT_RETURNTRANSFER => true,
