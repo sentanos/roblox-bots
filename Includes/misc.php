44f25f5b83bc9574fc1d661d372c19410270f476
FroastJ <froastj@gmail.com>
2015-04-27 20:31:11 -0400

    Move

--- Includes/misc.php
+++ Includes/misc.php
@@ -9,4 +9,18 @@
 	function getRankInGroup($userId,$groupId) {
 		return (int)simplexml_load_file("http://www.roblox.com/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRank&playerid=$userId&groupid=$groupId");
 	}
+	function fetchShout($cookie,$group) {
+		// Log in for correct permissions
+		$curl = curl_init("http://www.roblox.com/My/Groups.aspx?gid=$group");
+		curl_setopt_array($curl,array(
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_COOKIEFILE => $cookie,
+			CURLOPT_COOKIEJAR => $cookie
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+		$doc = new DOMDocument();
+		$doc->loadHTML($response);
+		return $doc->getElementById('ctl00_cphRoblox_GroupStatusPane_StatusTextField')->textContent;
+	}
 ?>
