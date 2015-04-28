b05153983180426970665fb6c69c1b3e1d4db3f4
FroastJ <froastj@gmail.com>
2015-04-27 20:27:15 -0400

    Update include file name

--- shout.php
+++ shout.php
@@ -1,5 +1,5 @@
 <?php
-	include_once'Includes/GetPostArray.php';
+	include_once'Includes/getPostArray.php';
 	function shout($cookie,$group,$msg) {
 		$url = "http://www.roblox.com/My/Groups.aspx?gid=$group";
 		$curl = curl_init($url);
