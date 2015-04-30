a0a75ae33c5a41fe852eb71bc1cbae51b8d7702a
FroastJ <froastj@gmail.com>
2015-04-29 20:02:16 -0400

    Fix send xml

--- upload.php
+++ upload.php
@@ -1,7 +1,6 @@
 <?php
 	/*
 	
-	UNTESTED - PENDING FOR TESTING
 	This is for uploading assets either by raw XML data or a string for uploading a LocalScript (ease for Script Builders).
 	You can use this in conjunction with getAsset to upload other (untrusted) assets into your model and ultimately your place.
 	Just use:
@@ -13,19 +12,18 @@
 	*/
 	function upload($cookie,$asset,$data,$local = false) {
 		if ($local) {
-			$send = "
-				<roblox xmlns:xmime='http://www.w3.org/2005/05/xmlmime' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='http://www.roblox.com/roblox.xsd' version='4'>
-					<External>null</External>
-					<External>nil</External>
-					<Item class='LocalScript'>
-						<Properties>
-							<bool name='Disabled'false</bool>
-							<Content name='LinkedSource'><null></null></Content>
-							<string name='Name'>Local</string>
-							<ProtectedString name='Source'><![CDATA[$data]]></ProtectedString>
-						</Properties>
-					</Item>
-				</roblox>";
+			$send = "<roblox xmlns:xmime='http://www.w3.org/2005/05/xmlmime' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='http://www.roblox.com/roblox.xsd' version='4'>
+						<External>null</External>
+						<External>nil</External>
+						<Item class='LocalScript'>
+							<Properties>
+								<bool name='Disabled'>false</bool>
+								<Content name='LinkedSource'><null></null></Content>
+								<string name='Name'>Local</string>
+								<ProtectedString name='Source'><![CDATA[$data]]></ProtectedString>
+							</Properties>
+						</Item>
+					</roblox>";
 		} else {
 			$send = $data;
 		}
