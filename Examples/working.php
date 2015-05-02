472c7e63b6bce4871a6cff28e03cb15a4e88fed7
FroastJ <froastj@gmail.com>
2015-05-02 16:55:39 -0400

    Rename, replace dirname

--- Examples/working.php
+++ Examples/working.php
@@ -1,15 +1,16 @@
 <?php
-	// This is simply an example file so that you know how to work everything (specifically logging in and cookies).
-	include_once dirname(dirname(__FILE__)).'/Includes/getRoles.php';
-	include_once dirname(dirname(__FILE__)).'/Includes/login.php';
-	include_once dirname(dirname(__FILE__)).'/changeRank.php';
+	/* This is simply an example file so that you know how to work everything (specifically logging in and cookies).
+	It is specifically made to automatically work directly from the examples folder. */
+	include_once '../Includes/getRoles.php';
+	include_once '../Includes/login.php';
+	include_once '../changeRank.php';
 	libxml_use_internal_errors(true); // Hide DOMDocument warnings (though your errors should be turned off anyways)
 	$group = 18; // Change this to your group ID
-	$cookieTime = dirname(dirname(__FILE__)).'/Private/cookieTime.txt';
+	$cookieTime = '../Private/cookieTime.txt';
 	if (!file_exists($cookieTime)) {
 		file_put_contents($cookieTime,0);
 	}
-	$cookie = dirname(dirname(__FILE__)).'/Private/cookie';
+	$cookie = '../Private/cookie';
 	if (time()-file_get_contents($cookieTime) > 86400) {
 		login($cookie,'Killer6199','mandie');
 		file_put_contents($cookieTime,time());
