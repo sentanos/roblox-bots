8c6c4b96034873942e0135c1ca79cf7919fb3422
FroastJ <froastj@gmail.com>
2015-05-22 20:20:07 -0400

    No header cutting
    No header was being returned in the first place

--- shout.php
+++ shout.php
@@ -1,5 +1,4 @@
 <?php
-	include_once'Includes/getPostArray.php';
 	function shout($cookie,$group,$msg) {
 		$url = "http://www.roblox.com/My/Groups.aspx?gid=$group";
 		$curl = curl_init($url);
@@ -10,7 +9,7 @@
 			CURLOPT_FOLLOWLOCATION => true
 		));
 		$response = curl_exec($curl);
-		$nextPost = getPostArray(substr($response,curl_getinfo($curl,CURLINFO_HEADER_SIZE)),
+		$nextPost = getPostArray($response,
 			array(
 				'ctl00$cphRoblox$GroupStatusPane$StatusTextBox' => $msg,
 				'ctl00$cphRoblox$GroupStatusPane$StatusSubmitButton' => 'Group Shout'	
